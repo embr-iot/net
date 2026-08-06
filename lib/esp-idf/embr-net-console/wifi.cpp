d9af5b4ec66261c26835bb61b7653fd9789fe31b
Malachi Burke <malachi.burke@gmail.com>
2026-08-06 09:08:52 -0700

    feat: autostart / status command work
    1. prep for auth ssid/pass (not ready yet)
    2. minor cleanup of preinit -> lazy_preinit
    3. bulk of autostart logic present, not ready yet
    4. 'status' command

--- lib/esp-idf/embr-net-console/wifi.cpp
+++ lib/esp-idf/embr-net-console/wifi.cpp
@@ -2,6 +2,7 @@
 
 #include <embr/esp-idf/wifi/fwd.h>
 
+#include <esp_check.h>
 #include <esp_console.h>
 #include <esp_wifi.h>
 #include <nvs_flash.h>
@@ -16,7 +17,10 @@ using string = const std::string_view;
 static embr::console::Args args;
 
 static const char* nvs_ns = "embr:net:con";
+static const char* nvs_prefs = "prefs";
+static const char* TAG = "embr::net::console";
 
+// TODO: Look into how much this overlaps with esp_wifi_restore
 struct
 {
     struct
@@ -34,13 +38,19 @@ static union
     {
         bool ap : 1;
         bool sta : 1;
+        bool preinit : 1;
         bool started : 1;   // DEBT: I think we can query wifi stack directly about this
+
+        //char ap_ssid[32];
+        //char ap_pass[32];
+        //char sta_ssid[32];
+        //char sta_pass[32];
     }   armed;
 
     unsigned armed_raw;
 };
 
-esp_err_t arm_start(wifi_mode_t mode = WIFI_MODE_NULL)
+esp_err_t arm_mode(wifi_mode_t mode = WIFI_MODE_NULL)
 {
     if(armed.ap || mode == WIFI_MODE_AP)
     {
@@ -65,6 +75,46 @@ esp_err_t arm_stop()
     return esp_wifi_stop();
 }
 
+static void print_prefs()
+{
+    printf("prefs\n");
+    printf("  autostart.ap=%u\n", prefs.autostart.ap);
+    printf("  autostart.sta=%u\n", prefs.autostart.sta);
+}
+
+static esp_err_t start()
+{
+    using namespace embr::wifi;
+
+    ESP_RETURN_ON_ERROR(arm_mode(), TAG, "Nothing to config");
+
+    if(armed.ap)
+    {
+        ESP_ERROR_CHECK(ap_init());
+    }
+    if(armed.sta)
+    {
+        ESP_ERROR_CHECK(sta_init());
+    }
+
+    ESP_ERROR_CHECK(esp_wifi_start());
+    armed.started = true;
+
+    return ESP_OK;
+}
+
+// DEBT: Clean up naming preinit is poorly named
+esp_err_t lazy_preinit()
+{
+    if(armed.preinit)   return ESP_OK;
+
+    ESP_RETURN_ON_ERROR(embr::wifi::preinit(), TAG, "preinit() call failed");
+
+    armed.preinit = true;
+
+    return ESP_OK;
+}
+
 static int wifi(int argc, char *argv[])
 {
     using namespace embr::wifi;
@@ -75,19 +125,28 @@ static int wifi(int argc, char *argv[])
 
     string command = args.command->sval[0];
     string arg1 = args.arg1->sval[0];
+    string arg2 = args.arg2->sval[0];
 
-    nvs_handle_t nvs;
-    esp_err_t ret = nvs_open(nvs_ns, NVS_READWRITE, &nvs);
+    if(command == "auth")
+    {
+        if(arg1 == "ssid")
+        {
+            // TBD
+        }
+        else if(arg1 == "pass")
+        {
+            // TBD
+        }
+        else
+            return -1;
 
-    ESP_ERROR_CHECK_WITHOUT_ABORT(ret);
-
-    if(ret == ESP_OK)   nvs_close(nvs);
-
-    if(command == "ap")
+        return 0;
+    }
+    else if(command == "ap")
     {
         if(arg1.empty() || arg1 == "arm")
         {
-            if(!armed_raw)  ESP_ERROR_CHECK(preinit());
+            ESP_ERROR_CHECK(lazy_preinit());
 
             armed.ap = true;
         }
@@ -99,9 +158,8 @@ static int wifi(int argc, char *argv[])
         }
         else if(arg1 == "start")
         {
-            if(!armed_raw)  ESP_ERROR_CHECK(preinit());
-
-            ESP_ERROR_CHECK(arm_start(WIFI_MODE_AP));
+            ESP_ERROR_CHECK(lazy_preinit());
+            ESP_ERROR_CHECK(arm_mode(WIFI_MODE_AP));
             ESP_ERROR_CHECK(ap_init());
             ESP_ERROR_CHECK(esp_wifi_start());
             armed.started = true;
@@ -113,36 +171,99 @@ static int wifi(int argc, char *argv[])
     }
     else if(command == "autostart")
     {
-        // Toggle autostart of either sta or ap depending on what's running now
+        // Set autostart of either sta or ap depending on what's running now
+        if(armed.started)
+        {
+            prefs.autostart.sta = armed.sta;
+            prefs.autostart.ap = armed.ap;
+        }
+        else
+        {
+            // If nothing running, that's our way of clearing out autostart
+            prefs.autostart.sta = false;
+            prefs.autostart.ap = false;
+        }
+
+        nvs_handle_t nvs;
+        esp_err_t ret = nvs_open(nvs_ns, NVS_READWRITE, &nvs);
+
+        ESP_RETURN_ON_FALSE(ret == ESP_OK, -1, TAG, "Couldn't open NVS");
+        ESP_RETURN_ON_FALSE(
+            nvs_set_blob(nvs, nvs_prefs, &prefs, sizeof(prefs)) == ESP_OK, -1, TAG,
+            "Couldn't write prefs");
+
+        nvs_close(nvs);
+        print_prefs();
+        return 0;
     }
     else if(command == "sta")
     {
         if(arg1.empty() || arg1 == "arm")
         {
-            if(!armed_raw)  ESP_ERROR_CHECK(preinit());
+            ESP_ERROR_CHECK(lazy_preinit());
             armed.sta = true;
             return 0;
         }
         else if(arg1 == "start")
         {
-            if(!armed_raw)  ESP_ERROR_CHECK(preinit());
+            ESP_ERROR_CHECK(lazy_preinit());
 
-            ESP_ERROR_CHECK(arm_start(WIFI_MODE_STA));
+            ESP_ERROR_CHECK(arm_mode(WIFI_MODE_STA));
             ESP_ERROR_CHECK(sta_init());
             ESP_ERROR_CHECK(esp_wifi_start());
             armed.started = true;
             return 0;
         }
     }
+    else if(command == "scan")
+    {
+        // TBD
+    }
     else if(command == "start")
     {
-        // shorthand for "sta start"
-        embr::wifi::simple_init();
+        if(armed_raw)
+        {
+            ESP_ERROR_CHECK(lazy_preinit());
+            start();
+        }
+        else
+            // shorthand for "sta start" - if nothing armed, that's our default
+            embr::wifi::simple_init();
+        return 0;
+    }
+    else if(command == "status")
+    {
+        wifi_mode_t mode;
+        int rssi;
+
+        puts("status");
+
+        if(armed.started)
+        {
+            ESP_ERROR_CHECK(esp_wifi_get_mode(&mode));
+
+            printf("  mode: %u\n", mode);
+            if(armed.sta)
+            {
+                ESP_ERROR_CHECK(esp_wifi_sta_get_rssi(&rssi));
+                printf("  rssi: %d\n", rssi);
+            }
+        }
+        else
+        {
+            printf("  not running\n");
+        }
+
+        print_prefs();
+
+        // TODO: Look into esp_wifi_statis_dump
+
         return 0;
     }
     else if(command == "stop")
     {
         ESP_ERROR_CHECK(esp_wifi_stop());
+        armed.started = false;
         return 0;
     }
 
@@ -159,7 +280,19 @@ esp_err_t wifi_console_init()
     if(ret == ESP_OK)
     {
         size_t len = sizeof(prefs);
-        ret = nvs_get_blob(nvs, "prefs", &prefs, &len);
+        ret = nvs_get_blob(nvs, nvs_prefs, &prefs, &len);
+
+        if(ret == ESP_OK)
+        {
+            armed.ap = prefs.autostart.ap;
+            armed.sta = prefs.autostart.sta;
+
+            if(armed_raw)
+            {
+                //arm_start();
+            }
+        }
+
         nvs_close(nvs);
     }
 
@@ -175,7 +308,7 @@ esp_err_t wifi_console_init()
     };
 
     args.command = arg_str1(nullptr, nullptr,
-        "<cred|sta|ap|start|stop>",
+        "<auth|autostart|sta|ap|start|status|stop>",
         "");
     args.arg1 = arg_strn(nullptr, nullptr,
         "<ssid|pass|start|stop>",
